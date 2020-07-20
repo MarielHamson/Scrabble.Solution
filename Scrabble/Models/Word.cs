0882115db666425056d878ffe2c2ec05759f4e55
Allison Sadin <aesadin@gmail.com>
2020-07-20 15:59:32 -0700

    adjusted constructor, created dictionary and started method to add letter values to user input
    Co-authored-by: MarielHamson <mariel.hamson@gmail.com>

--- Scrabble/Models/Word.cs
+++ Scrabble/Models/Word.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -7,10 +8,22 @@ namespace Word.Models
   public class WordChecker
   {
     public string Input { get; set; }
+    public int Points { get; set; }
+    private static Dictionary<int, List<string>> scrabbleDic = new Dictionary<int, List<string>>()
+    {
+      { 1, new List<string> {"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"} },
+      { 2, new List<string> {"d", "g"} },
+      { 3, new List<string> {"b", "c", "m", "p"} },
+      { 4, new List<string> {"f", "h", "v", "w", "y"} },
+      { 5, new List<string> {"k"} },
+      { 8, new List<string> {"j", "x"} },
+      { 10, new List<string> {"q", "z"} }
+    };
 
     public WordChecker(string input)
     {
       Input = input;
+      Points = 0;
     }
 
     public bool IsOnlyAlphaCharacters()
@@ -22,6 +35,17 @@ namespace Word.Models
       else
       {
         return false;
+      }
+    }
+
+    public int AddLettersOfWord()
+    {
+      // forEach(letter in word)
+      {
+        if (scrabbleDic[])
+        {
+          this.Points += 1;
+        }
       }
     }
   }
